c7d4a56130060bfb59b029949422aa3314d47ee8
justin <justin.ethier@gmail.com>
2011-03-14 00:38:26 -0400

    New tests

---
+++ test2.js
@@ -0,0 +1,19 @@
+/**
+ * Test to demonstrate search on a large dataset.
+ *
+ * This file is part of node-kdtree, a node.js Addon for working with kd-trees.
+ * Copyright (C) 2011 Justin Ethier <justinethier@github>
+ *
+ * Please use github to submit patches and bug reports:
+ * https://github.com/justinethier/node-kdtree
+ */
+var kd = require('kdtree');
+var tree = new kd.KDTree();
+
+console.log( "Loading data into tree..." );
+for (var x = 0; x < 75; x++){
+  for (var y = 0; y < 75; y++){
+    for (var z = 0; z < 75; z++){
+      tree.insert(x, y, z); }}}
+console.log( "Done loading the tree" );
+console.log( tree.nearest(9.5, 19.5, 31.5) );
