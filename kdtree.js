5e8736393f6cb2631a63519cf0bdc05f81fc7c50
justin <justin.ethier@gmail.com>
2011-03-15 13:10:31 -0400

    Removed old file

--- kdtree.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file is part of node-kdtree, a node.js Addon for working with kd-trees.
- * Copyright (C) 2011 Justin Ethier <justinethier@github>
- *
- * Please use github to submit patches and bug reports:
- * https://github.com/justinethier/node-kdtree
- */
-var klib = require("kdtree");
-module.exports = new klib.KDTree;
