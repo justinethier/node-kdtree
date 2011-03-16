4f0403c82c23c0dd0c4b914bf3b2287584e7afe6
justin <justin.ethier@gmail.com>
2011-03-16 17:46:35 -0400

    Added assertion tests

--- tests/test.js
+++ tests/test.js
@@ -5,6 +5,7 @@
  * Please use github to submit patches and bug reports:
  * https://github.com/justinethier/node-kdtree
  */
+var assert = require('assert');
 var kd = require('kdtree');
 var tree = new kd.KDTree();
 // TODO: put all  insert data into a big list, and print that list out here...
@@ -26,8 +27,13 @@ var tree2 = new kd.KDTree(4);
 tree2.insert(10, 10, 10, 10, "10x4 test");
 tree2.insert(1, 2, 3, 4, "A 4-dimensional point");
 tree2.insert(1, 1.9, 3, 4, "Another 4-dimensional point");
-console.log( tree2.nearest(10, 10, 10, 10) );
-console.log( tree2.nearest(1, 10, 10, 1) );
+assert.deepEqual(tree2.nearest(10, 10, 10, 10),
+                 [ 10, 10, 10, 10, '10x4 test' ],
+                 "Searching on a new tree");
+assert.deepEqual(tree2.nearest(1, 10, 10, 1),
+                 [1, 2, 3, 4, "A 4-dimensional point"]);
 
 // Test that first tree still works...
-console.log( tree.nearest(10, 10, 10) );
+assert.deepEqual( tree.nearest(10, 10, 10),
+                  [ 1, 11.1, 4 ],
+                  "Verify original tree can still be searched");
